9d77a6b995e62e1184e41f5f5a506875f99ccb58
prangunj23 <prangunj23@ncs.charter.k12.de.us>
2021-09-02 20:45:00 -0400

    Code

--- BPAPractice/BPAPractice2014.cpp
+++ BPAPractice/BPAPractice2014.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+
+        void display_inventory() {
+            string d; 
+            ifstream myFile("gameinventory.txt");
+            while (getline(myFile, d)) {
+                cout << d << endl;
+        }
+        }
+        void add_item() {
+            cout << "Enter the name of this new item: " << endl;
+            string x; cin >> x;
+
+            string d;
+            ifstream myFile("gameinventory.txt");
+            
+        }
+
+
+
 int main() {
     
-}
+    cout << endl << "#################################################" << endl;
+    cout << "Game Item Inventory" << endl;
+    cout << "##################################################" << endl;
+    cout << "[d] Display all the inventory from file." << endl;
+    cout << "[a] Append an item to the inventory file." << endl;
+    cout << "[q] Quit and commit changes to file." << endl;
+    cout << "Choice (enter upper or lowercase letter option): ";
+    string x; cin >> x;
+     if (x == "d" || x == "D") {
+        display_inventory();
+    }   
+    }
+
+    
